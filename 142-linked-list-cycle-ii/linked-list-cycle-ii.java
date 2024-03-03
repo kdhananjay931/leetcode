51f12b08ade231f25a495b2aecf17d98a1822a87
Dhananjay Kumar <kdhananjay931@gmail.com>
2024-03-03 17:18:32 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (28.36%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -22,6 +22,7 @@ public class Solution {
             if (slow == fast) {
                 slow= head;
             
+            
             while(slow!=fast){
             slow=slow.next;
             fast=fast.next;
